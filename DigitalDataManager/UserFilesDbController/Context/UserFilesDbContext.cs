b990bdf011da23cc1fe56b8f4195ba86e77d3ab4
Aliaksei Kazlou <aliaksei_kazlou@epam.com>
2014-11-27 15:29:28 +0300

    add new DataBase - UserFilesDbController

---
+++ DigitalDataManager/UserFilesDbController/Context/UserFilesDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserFilesDbController.Tables;
+
+namespace UserFilesDbController.Context
+{
+    public class UserFilesDbContext : DbContext
+    {
+        public DbSet<UserTable> Users { get; set; }
+        public DbSet<AlbumTable> Albums { get; set; }
+        public DbSet<FileTable> Files { get; set; }
+    }
+}
