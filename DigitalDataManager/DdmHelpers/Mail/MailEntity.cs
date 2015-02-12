2bb78d0abd4eccd135370a1add73a2f473dfcbbf
Aliaksei Kazlou <aliaksei_kazlou@epam.com>
2015-02-12 17:32:35 +0300

    add "MailHelper"

---
+++ DigitalDataManager/DdmHelpers/Mail/MailEntity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DdmHelpers.Mail
+{
+    class MailEntity
+    {
+        public string Subject { get; private set; }
+        public string Message { get; private set; }
+
+        private readonly List<string> _mailsTo;
+        private readonly List<string> _attachFiles;
+
+        public MailEntity(string subject, string message, string mailTo)
+        {
+            Subject = subject;
+            Message = message;
+
+            _mailsTo = new List<string>();
+            _attachFiles = new List<string>();
+
+            _mailsTo.Add(mailTo);
+        }
+
+        public List<string> MailsTo
+        {
+            get { return _mailsTo; }
+        }
+
+        public List<string> AttachFiles
+        {
+            get { return _attachFiles; }
+        }
+    }
+}
---
+++ DigitalDataManager/DdmHelpers/Mail/MailHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DdmHelpers.Mail
+{
+    class MailHelper
+    {
+        public string SmtpServer { get; private set; }
+        public string MailFrom { get; private set; }
+        public string MailPassword { get; private set; }
+
+        private readonly SmtpClient _client;
+
+        public MailHelper(string smtpServer, string mailFrom, string mailPassword)
+        {
+            SmtpServer = smtpServer;
+            MailFrom = mailFrom;
+            MailPassword = mailPassword;
+
+            _client = new SmtpClient
+            {
+                Host = smtpServer,
+                Port = 587,
+                EnableSsl = true,
+                Credentials = new NetworkCredential(MailFrom.Split('@')[0], mailPassword),
+                DeliveryMethod = SmtpDeliveryMethod.Network
+            };
+        }
+
+        public void SendMail(MailEntity entity)
+        {
+            using (var mail = ToMailMessage(entity))
+            {
+                _client.Send(mail);
+            }
+        }
+
+        private MailMessage ToMailMessage(MailEntity entity)
+        {
+            var mail = new MailMessage
+            {
+                From = new MailAddress(MailFrom),
+                Subject = entity.Subject,
+                Body = entity.Message
+            };
+
+            foreach (var item in entity.MailsTo)
+            {
+                mail.To.Add(new MailAddress(item));
+            }
+
+            foreach (var item in entity.AttachFiles)
+            {
+                mail.Attachments.Add(new Attachment(item));
+            }
+
+            return mail;
+        }
+    }
+}
